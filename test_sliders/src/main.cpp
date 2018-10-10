0d1d702c4d741a795097975da3a862ea6ee81c76
Bentleyj <jgbentley10@gmail.com>
2018-10-10 16:56:36 +0100

    Make screens unadorned and spread across multiple screens

--- test_sliders/src/main.cpp
+++ test_sliders/src/main.cpp
@@ -3,7 +3,14 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofGLFWWindowSettings settings;
+	settings.width = 1080 + 1080;
+	settings.height = 1920;
+	settings.decorated = false;
+	settings.windowMode = OF_WINDOW;
+
+	ofCreateWindow(settings);
+	//ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
